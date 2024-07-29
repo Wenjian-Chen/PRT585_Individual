d411b60fbee94844b55f35269e40827ecfd7eca0
Wenjian-Chen <wjchen1001@gmail.com>
2024-07-30 03:08:16 +0930

    add search

--- week1HelloWorldMVC/MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ week1HelloWorldMVC/MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671f3fe65bcbd8979bb045d16aedf9756ef14712")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd74dcb761f76b96a09425381f8de57288fb146")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
