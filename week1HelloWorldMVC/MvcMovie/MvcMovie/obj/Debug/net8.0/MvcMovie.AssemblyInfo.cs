90be4958e440b969d432fde26823fd9d1c7369d8
Wenjian-Chen <wjchen1001@gmail.com>
2024-07-30 03:27:46 +0930

    add validation

--- week1HelloWorldMVC/MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ week1HelloWorldMVC/MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d411b60fbee94844b55f35269e40827ecfd7eca0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c7bc724bd4a3fba6fb8d0295ba1283c6c89b8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
